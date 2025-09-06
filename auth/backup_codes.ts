5f89f0055f0e7071868cd3d6e4bbbf6d2575fc17
henryc1551 <henrykleon1551@gmail.com>
2025-09-06 23:12:57 +0100

    Create backup_codes.ts

---
+++ auth/backup_codes.ts
@@ -0,0 +1,4 @@
+export function generateCodes(){ return []; }
+export async function saveCodes() {}
+export async function consumeCode(){ return false; }
+export async function countCodes(){ return 0; }
